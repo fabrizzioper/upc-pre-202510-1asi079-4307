e82e180e74bed521115f063cd11e11b206b26784
Fabrizzio Pereira <fabrizzioper@gmail.com>
2025-05-02 01:35:08 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'pc1-1ASI0729-u202417468';
+}
